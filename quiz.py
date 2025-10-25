e158c39c1590b7865a911b83e8ffbebab23b5568
sonika729190 <sonikayadav131999@gmail.com>
2025-10-25 18:08:37 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,3 +1,126 @@
-import streamlit as st
+#Quiz Game:-
+print("Q1: First Alphabet of English language..?\nA) d\tb) e\nc) a\td) f")
+ans1=input("Enter your answer here...")
+print("your answer is ...",ans1)
+print("<.......................>")
+print("Q2:which of these is a mammal..?\nA) Duck\tB) hen\nC) owl\tD whale")
+ans2=input("enter your answer here...")
+print("your answer is ...",ans2)
+print("<......................>")
+print("Q3:Which is the largest ocean in world?\nA) Indian ocean\tb) Pacific ocean\nC) Arctic ocean\tD) Atlantic ocean(")
+ans3=input("enter your answer here...")
+print("your answer is ...",ans3)
+print("..............>")
+print("Q4: Which is the national Animal..?\nA) fox\tB Cow\nC) Tiger\tD) Elephant")
+ans4=input("enter your answer here...")
+print("your answer is ...",ans4)
+print("<......................>")
+print("Q5: How many continent in the world..?\nA) 4\tB) 7\nC) 8\nD) 9")
+ans5=input("enter your answer here...")
+print("your answer is ...",ans5)
+print("<......................>")
+print("Q6: which country is most population...?\nA) india\tB) america\tC) china\tD) russia")
+ans6=input("enter your answer here ....")
+print("your answer is....,ans6")
+print("<............>")
+print("Q7: who discovered gravity...?\nA) Newton\tb) Robert hoke\tC) charles louis\tD) Herry moseley ")
+ans7=input("enter your answer here...")
+print("<...........>")
+print("Q8: who discovered zero...?\nA) ramanujan\tB) brahmagupta\tC) arbitrary\tD) bhaskara")
+ans8=input("enter your answer here...")
+print("<.............>")
+print("Q9: which vitamin present in milk....? \nA) vitaminB\tB) vitaminA\tC) vitaminC\tD) vitaminD")
+ans9=input("enter your answer here...")
+print("<............>")
+print("Q10: who discovered plasmodium....?\nA) robert hoke\tB) herry moseley\tC) bhaskara\tD) charles louis")
+ans10=input("enter your answer here....")
+print("<..............>")
+Total=0
+if ans1=="c" or ans1== "C":
+    Total+=1
+if ans2=="d" or ans2=="D":
+    Total+=1
+if ans3=="b" or ans3=="B":
+    Total+=1
+if ans4=="c" or ans4=="c":
+    Total+=1
+if ans5=="b" or ans5=="B":
+    Total+=1lo
+if ans6=="a"or ans6=="A":
+    Total+=1
+if ans7=="a" or ans7=="A":
+    Total+=1
+if ans8=="c" or ans8=="C":
+    Total+=1
+if ans9=="b" or ans9=="B":
+    Total+=1
+if ans10=="d" or ans10=="D":
+    Total+=1
+print(Total)
+if Total==10:
+    print("congratulations you are the topper with 10 marks.....")
+elif Total==8:
+    print("congratulations yo have scored second position......")
+elif Total==6:
+    print("congratulations you have scored third position.......")
+else:
+    print("better next time...")
 
-st.balloons()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
